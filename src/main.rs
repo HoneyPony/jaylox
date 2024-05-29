c81bfd376a75f4bbb72b4e93077f9a2ccee1a5a2
HoneyPony <honeyponygames@gmail.com>
2024-05-29 17:17:17 -0400

    Tokens part 1

---
+++ src/main.rs
@@ -0,0 +1,84 @@
+mod scanner;
+
+use std::env;
+use std::process::exit;
+use std::io;
+use std::io::Write;
+
+use scanner::Scanner;
+
+struct Lox {
+	had_error: bool
+}
+
+impl Lox {
+
+	fn new() -> Self {
+		Lox {
+			had_error: false,
+		}
+	}
+
+	fn report(&mut self, line: i32, where_: &str, message: &str) {
+		eprintln!("[line {line}] Error{where_}: {message}");
+		self.had_error = true;
+	}
+
+	fn error(&mut self, line: i32, message: &str) {
+		self.report(line, "", message);
+	}
+
+	fn run(&mut self, code: String) {
+		let tokens = {
+			let mut scanner = Scanner::new(code, self);
+			scanner.scan_tokens()
+		};
+
+		for token in tokens {
+			println!("{}", token.to_string());
+		}
+	}
+
+	fn run_file(&mut self, path: String) -> std::io::Result<()> {
+		let contents = std::fs::read_to_string(path)?;
+		self.run(contents);
+		Ok(())
+	}
+
+	fn run_prompt(&mut self) {
+		loop {
+			print!("> ");
+			let _ = io::stdout().flush();
+
+			let mut line = String::new();
+			let Ok(_) =io::stdin().read_line(&mut line) else {
+				break;
+			};
+
+			self.run(line);
+			self.had_error = false;
+		}
+	}
+
+}
+
+fn main() -> io::Result<()> {
+	let mut args: Vec<String> = env::args().collect();
+	let mut lox = Lox::new();
+
+	if args.len() > 2 {
+		println!("Usage: jlox [script]");
+		exit(64);
+	}
+	else if args.len() == 2 {
+		lox.run_file(args.remove(1))?;
+		if(lox.had_error) {
+			exit(65);
+		}
+	}
+	else {
+		lox.run_prompt();
+	}
+
+	exit(0);
+}
---
+++ src/scanner.rs
@@ -0,0 +1,134 @@
+use std::str::Chars;
+
+use crate::Lox;
+
+#[derive(PartialEq, Eq, Debug)]
+pub enum TokenType {
+	LeftParen, RightParen, LeftBrace, RightBrace,
+	Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
+
+	Bang, BangEqual,
+	Equal, EqualEqual,
+	Greater, GreaterEqual,
+	Less, LessEqual,
+
+	Idenitifer, String, Number,
+
+	And, Class, Else, False, Fun, For, If, Nil, Or,
+	Print, Return, Super, This, True, Var, While,
+
+	Eof
+}
+
+pub enum TokenLiteral {
+	None,
+	String(String),
+	Number(f64)
+}
+
+impl ToString for TokenLiteral {
+	fn to_string(&self) -> String {
+		match self {
+			TokenLiteral::None => "None".to_string(),
+			TokenLiteral::String(what) => what.clone(),
+			TokenLiteral::Number(num) => num.to_string(),
+		}
+	}
+}
+
+pub struct Token {
+	typ: TokenType,
+	lexeme: String,
+	literal: TokenLiteral,
+	line: i32
+}
+
+impl Token {
+	pub fn new(typ: TokenType, lexeme: String, literal: TokenLiteral, line: i32) -> Self {
+		Token { typ, lexeme, literal, line }
+	}
+}
+
+impl ToString for Token {
+	fn to_string(&self) -> String {
+		return format!("{0:?} {1} {2}", self.typ, self.lexeme, self.literal.to_string());
+	}
+}
+
+pub struct Scanner<'a> {
+	source: String,
+	chars: Vec<char>,
+	start: i32,
+	current: i32,
+	line: i32,
+
+	lox: &'a mut Lox
+}
+
+impl<'a> Scanner<'a> {
+	pub fn new(source: String, lox: &'a mut Lox) -> Scanner {
+		let chars = source.chars().collect();
+
+		Scanner {
+			source,
+			chars,
+			start: 0,
+			current: 0,
+			line: 1,
+
+			lox
+		}
+	}
+
+	fn is_at_end(&self) -> bool {
+		return self.current >= self.source.len() as i32
+	}
+
+	fn advance(&mut self) -> char {
+		let result: char = self.chars[self.current as usize];
+		self.current += 1;
+		result
+	}
+
+	fn add_token(&mut self, tokens: &mut Vec<Token>, typ: TokenType) {
+		self.add_token_lit(tokens, typ, TokenLiteral::None);
+	}
+
+	fn add_token_lit(&mut self, tokens: &mut Vec<Token>, typ: TokenType, lit: TokenLiteral) {
+		let text = &self.chars[self.start as usize..self.current as usize];
+		tokens.push(Token::new(typ, String::from_iter(text), lit, self.line));
+	}
+
+	fn scan_token(&mut self, tokens: &mut Vec<Token>) {
+		let c = self.advance();
+		match c {
+			'(' => { self.add_token(tokens, TokenType::LeftParen); }
+			')' => { self.add_token(tokens, TokenType::RightParen); }
+			'{' => { self.add_token(tokens, TokenType::LeftBrace); }
+			'}' => { self.add_token(tokens, TokenType::RightBrace); }
+			',' => { self.add_token(tokens, TokenType::Comma); }
+			'.' => { self.add_token(tokens, TokenType::Dot); }
+			'-' => { self.add_token(tokens, TokenType::Minus); }
+			'+' => { self.add_token(tokens, TokenType::Plus); }
+			';' => { self.add_token(tokens, TokenType::Semicolon); }
+			'*' => { self.add_token(tokens, TokenType::Star); }
+
+			_ => {
+				self.lox.error(self.line, &format!("Unexpected character '{}'", c));
+			}
+		}
+	}
+
+	pub fn scan_tokens(&mut self) -> Vec<Token> {
+		let mut tokens = vec![];
+
+		while(!self.is_at_end()) {
+			self.start = self.current;
+			self.scan_token(&mut tokens);
+		}
+
+		tokens.push(Token::new(TokenType::Eof, "".to_string(), TokenLiteral::None, self.line));
+
+		return tokens;
+	}
+}
