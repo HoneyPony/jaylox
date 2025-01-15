fe8c8cc19960c02b78a766b07b7a7ecf0fe82590
HoneyPony <honeyponygames@gmail.com>
2025-01-14 22:59:11 -0500

    Implement infrastructure to use our own tests.
    This seems nice.

--- fetch-tester.sh
+++ fetch-tester.sh
@@ -1,3 +1,12 @@
-git clone https://github.com/munificent/craftinginterpreters extern/craftinginterpreters --depth 1
+if [ -d extern/craftinginterpreters ]; then
+    # Repo is already cloned, just setup our own tests.
+    # Delete the directory first in case we deleted some tests.
+    rm -rf extern/craftinginterpreters/test/jaylox
+    cp -r -T tests extern/craftinginterpreters/test/jaylox
+else
+    git clone https://github.com/munificent/craftinginterpreters extern/craftinginterpreters --depth 1
+fi
+
+# Prepare the tester in case it wasn't prepared yet.
 cd extern/craftinginterpreters
 make get
